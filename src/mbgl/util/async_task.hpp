a958279b77e3e23a5e74a3131711c8bf4d02fc93
Thiago Marcos P. Santos <thiago@mapbox.com>
2015-08-31 10:41:52 +0300

    [core] Introduce the AsyncTask

---
+++ src/mbgl/util/async_task.hpp
@@ -0,0 +1,30 @@
+#ifndef MBGL_UTIL_ASYNC_TASK
+#define MBGL_UTIL_ASYNC_TASK
+
+#include <mbgl/util/noncopyable.hpp>
+
+#include <memory>
+#include <functional>
+
+namespace mbgl {
+namespace util {
+
+class AsyncTask : private util::noncopyable {
+public:
+    AsyncTask(std::function<void()>&&);
+    ~AsyncTask();
+
+    void send();
+    void unref();
+
+    void setThrottle(unsigned msec);
+
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl;
+};
+
+}
+}
+
+#endif
