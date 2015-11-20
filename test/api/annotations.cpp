01d544f9313dc60c7a38c12319a39ed5650eb7b9
John Firebaugh <john.firebaugh@gmail.com>
2015-11-19 16:07:15 -0800

    [tests] Don't use icons from the style for annotations

--- test/api/annotations.cpp
+++ test/api/annotations.cpp
@@ -16,6 +16,11 @@
 
 using namespace mbgl;
 
+std::shared_ptr<SpriteImage> defaultMarker() {
+    util::Image image(util::read_file("test/fixtures/sprites/default_marker.png"));
+    return std::make_shared<SpriteImage>(image.getWidth(), image.getHeight(), 1.0, std::string(image.getData(), image.getWidth() * image.getHeight() * 4));
+}
+
 std::string renderPNG(Map& map) {
     std::promise<std::unique_ptr<const StillImage>> promise;
     map.renderStill([&](std::exception_ptr, std::unique_ptr<const StillImage> image) {
@@ -33,6 +38,7 @@ TEST(Annotations, PointAnnotation) {
 
     Map map(view, fileSource, MapMode::Still);
     map.setStyleJSON(util::read_file("test/fixtures/api/empty.json"), "");
+    map.setSprite("default_marker", defaultMarker());
     map.addPointAnnotation(PointAnnotation({ 0, 0 }, "default_marker"));
 
     util::write_file("test/output/point_annotation.png", renderPNG(map));
@@ -97,6 +103,7 @@ TEST(Annotations, AddMultiple) {
 
     Map map(view, fileSource, MapMode::Still);
     map.setStyleJSON(util::read_file("test/fixtures/api/empty.json"), "");
+    map.setSprite("default_marker", defaultMarker());
     map.addPointAnnotation(PointAnnotation({ 0, -20 }, "default_marker"));
 
     renderPNG(map);
@@ -133,6 +140,7 @@ TEST(Annotations, RemovePoint) {
 
     Map map(view, fileSource, MapMode::Still);
     map.setStyleJSON(util::read_file("test/fixtures/api/empty.json"), "");
+    map.setSprite("default_marker", defaultMarker());
     uint32_t point = map.addPointAnnotation(PointAnnotation({ 0, 0 }, "default_marker"));
 
     renderPNG(map);
@@ -183,6 +191,7 @@ TEST(Annotations, SwitchStyle) {
 
     Map map(view, fileSource, MapMode::Still);
     map.setStyleJSON(util::read_file("test/fixtures/api/empty.json"), "");
+    map.setSprite("default_marker", defaultMarker());
     map.addPointAnnotation(PointAnnotation({ 0, 0 }, "default_marker"));
 
     renderPNG(map);
@@ -190,17 +199,4 @@ TEST(Annotations, SwitchStyle) {
     map.setStyleJSON(util::read_file("test/fixtures/api/empty.json"), "");
 
     util::write_file("test/output/switch_style.png", renderPNG(map));
-}
-
-TEST(Annotations, CustomIcon) {
-    auto display = std::make_shared<mbgl::HeadlessDisplay>();
-    HeadlessView view(display, 1);
-    DefaultFileSource fileSource(nullptr);
-
-    Map map(view, fileSource, MapMode::Still);
-    map.setStyleJSON(util::read_file("test/fixtures/api/empty.json"), "");
-    map.setSprite("cafe", std::make_shared<SpriteImage>(12, 12, 1, std::string(12 * 12 * 4, '\xFF')));
-    map.addPointAnnotation(PointAnnotation({ 0, 0 }, "cafe"));
-
-    util::write_file("test/output/custom_icon.png", renderPNG(map));
 }
