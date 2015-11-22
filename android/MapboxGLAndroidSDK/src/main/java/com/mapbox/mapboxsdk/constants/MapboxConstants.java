5e719ba2381ae76c6660f95b923fe197b89d529c
Tobrun <tobrun.van.nuland@gmail.com>
2015-11-22 12:01:54 +0100

    Consistent documentation style in constants package

--- android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
+++ android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
@@ -4,10 +4,14 @@ import java.util.Locale;
 
 public class MapboxConstants {
 
-    // Default Locale for data processing (ex: String.toLowerCase(MAPBOX_LOCALE, "foo"))
+    /**
+     * Default Locale for data processing (ex: String.toLowerCase(MAPBOX_LOCALE, "foo"))
+     */
     public static final Locale MAPBOX_LOCALE = Locale.US;
 
-    // Key used to store access token in AndroidManifest.xml
+    /**
+     * Key used to store access token in AndroidManifest.xml
+     */
     public static final String KEY_META_DATA_MANIFEST = "com.mapbox.AccessToken";
 
 }
